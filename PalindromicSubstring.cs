546d5bcadfdd1e15e2b2006224b840293e7a65cb
Thaynara Ferreira <thaynara@ice.ufjf.br>
2024-05-20 23:59:24 -0300

    adding solution and readme

---
+++ PalindromicSubstring.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class MainClass
+{
+    public static string PalindromicSubstring(string str)
+    {
+        string longestPalindromic = String.Empty;
+        char[] word = str.ToCharArray();
+        char[] reversedWord = new char[word.Length];
+        Array.Copy(word, reversedWord, word.Length);
+        Array.Reverse(reversedWord);
+        for (int palindromicSize = 3; palindromicSize <= word.Length; palindromicSize++)
+        {
+            for (int i = 0; i < word.Length - palindromicSize - 1; i++)
+            {
+                String potentialPalindromic = new String(word, i, palindromicSize);
+                String potentialPalindromicAux = new String(reversedWord, word.Length - i - palindromicSize, palindromicSize);
+                if (potentialPalindromic == potentialPalindromicAux)
+                {
+                    longestPalindromic = potentialPalindromic;
+                }
+            }
+        }
+        return longestPalindromic.Length > 2 ? longestPalindromic : "none";
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(PalindromicSubstring(Console.ReadLine()));
+    }
+}
